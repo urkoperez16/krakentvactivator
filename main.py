eec7f5a39e346d319a4538d39b873466cbb7eeb7
urkoperez16 <105135600+urkoperez16@users.noreply.github.com>
2023-08-27 17:07:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Ruta al controlador descargado
 driver_path = 'YOUR PATH'
 
 def interact_with_buttons():
-    driver = webdriver.Chrome(executable_path=driver_path)
-    
-    url = 'https://www.listasiptvactualizadas.com/activar-kraken-tv.php?token=YOUR TOKEN'
+    # Configura las opciones del controlador
+    chrome_options = Options()
+
+    # Inicializa el controlador
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Abre la página
+    url = 'https://listasiptvactualizadas.com/activar-kraken-tv.php?token=YOUR TOKEN'
     driver.get(url)
     
-    wait = WebDriverWait(driver, 15)
-    element = wait.until(EC.presence_of_element_located((By.ID, 'YOUR BOTTOM')))
+    wait = WebDriverWait(driver, 30)
+    element = wait.until(EC.presence_of_element_located((By.ID, 'YOUR BUTTON ID')))
 
-    button1 = driver.find_element(By.ID, 'YOUR BOTTOM')
+    button1 = driver.find_element(By.ID, 'YOUR BUTTON ID')
     button1.click()
+
+    # Espera 5 segundos
+    time.sleep(5)
+
+    # If you have 2 devices delete #
+    #button2 = driver.find_element(By.ID, 'YOUR BUTTON ID')
+    #button2.click()
 
     time.sleep(2)
 
-    # If you have 2 devices delete next #
-    #button2 = driver.find_element(By.ID, 'YOUR BOTTOM 2')
-    #button2.click()
-    #time.sleep(5)
-
     driver.quit()
 
-interact_with_buttons()
-
 while True:
-    time.sleep(1800)  # 1800 segundos = 30 minutos
     interact_with_buttons()
+    time.sleep(1800)
